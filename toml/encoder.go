d4bb89a311caee13c8fd4d908a4a70d52a62db91
Stephen Levine <stephen.levine@gmail.com>
2020-08-01 00:31:59 -0400

    Fix TOML encoder method signature

--- toml/encoder.go
+++ toml/encoder.go
@@ -15,7 +15,7 @@ type Encoder struct {
 	NaN, PosInf, NegInf interface{}
 }
 
-func (e *Encoder) Encode(normal interface{}) (yaml interface{}, err error) {
+func (e *Encoder) Encode(normal interface{}) (toml interface{}, err error) {
 	defer catchFailure(&err)
 	et := encodeTracker{Encoder: e}
 	return et.denormalize(normal), nil
